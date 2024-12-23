96f35f7a41425c6bbae695af1c28c5588d019903
ladianchad <qhrejddlvltm@gmail.com>
2024-12-23 23:41:54 +0900

    add init
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-const App = () => {
+import {Route, Router} from 'wouter';
+import Navigator from "./components/Navigator";
+import Home from "./pages/home/Home";
 
-  return <></>
+const App = () => {
+    return <Router>
+        <Navigator/>
+        <Route path="/" component={Home}/>
+        <Route path="/cv" component={Home}/>
+        <Route path="/projects" component={Home}/>
+    </Router>
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 
 export default defineConfig({
-  base: '/toefl_timer/',
+  base: '/',
   server: {
     host: true,
     port: 3000
